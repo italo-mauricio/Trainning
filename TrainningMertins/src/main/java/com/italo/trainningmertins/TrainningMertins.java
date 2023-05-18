da533231456a387b5e4e66ce127a6df0444229e8
italo <italogithub@gmail.com>
2023-05-18 09:23:08 -0300

    update

---
+++ TrainningMertins/src/main/java/com/italo/trainningmertins/TrainningMertins.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.italo.trainningmertins;
+
+/**
+ *
+ * @author italo
+ */
+public class TrainningMertins {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
