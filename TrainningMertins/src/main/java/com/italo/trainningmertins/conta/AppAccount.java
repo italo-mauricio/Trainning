aa5079ae22d6d09b11974cea14c42f297cdaf8a8
italo <italogithub@gmail.com>
2023-05-18 10:11:58 -0300

    update

--- TrainningMertins/src/main/java/com/italo/trainningmertins/conta/AppAccount.java
+++ TrainningMertins/src/main/java/com/italo/trainningmertins/conta/AppAccount.java
@@ -4,16 +4,50 @@
  */
 package com.italo.trainningmertins.conta;
 
+import java.util.Scanner;
+
 /**
  *
  * @author italo
  */
 public class AppAccount {
     
-    private static String loop(){
-        String soon = "Welcome to Italo Bank";
+     private static String soon(){
+        String soon = "Welcome to Italo Bank!";
         System.out.println(soon);
         return soon;
     }
     
+     public static String newPassword() {
+        Scanner scan = new Scanner(System.in);
+        String newPassword;
+        boolean isValid = false;
+        
+        while (!isValid) {
+            System.out.println("Create a new password [numbers and letters]: ");
+            newPassword = scan.next();
+            
+            if (isValidPassword(newPassword)) {
+                isValid = true;
+                System.out.println("New password successfully registered!");
+                return newPassword;
+            } else {
+                System.out.println("Invalid password! Please use only numbers and letters.");
+            }
+        }
+        
+        return null;
+    }
+    
+    private static boolean isValidPassword(String password) {
+        // Verifique se a senha contém apenas letras e números
+        return password.matches("[a-zA-Z0-9]+");
+    }
+    
+    public static void main(String[] args) {
+        Account account = new Account();
+        
+    }
+
+
 }
