db8d6983f03807c9dac6f3250579f0de46249e74
italo <italogithub@gmail.com>
2023-05-19 19:21:17 -0300

    em desenvolvimento

---
+++ TrainningMertins/src/main/java/com/italo/trainningmertins/exercicios/slide1/Ex04.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.italo.trainningmertins.exercicios.slide1;
+
+/**
+ *
+ * @author italo
+ */
+public class Ex04 {
+    
+    public class Matematica{
+        
+        
+        private int valor;
+
+        /**
+         * @return the valor
+         */
+        public int getValor() {
+            return valor;
+        }
+
+        /**
+         * @param valor the valor to set
+         */
+        public void setValor(int valor) {
+            this.valor = valor;
+        }
+        
+        public void dobra(){
+            int valor * 2;
+        }
+       
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        Matematica mat = new Matematica();
+    }
+}
