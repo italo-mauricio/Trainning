98c88354e4038805047eec6df60ac2d59f6cee6d
italo <italogithub@gmail.com>
2023-05-22 09:42:05 -0300

    exeptions

---
+++ TrainningMertins/src/main/java/com/italo/trainningmertins/exercicios/slide1/TesteException.java
@@ -0,0 +1,40 @@
+
+/**
+ *
+ * @author mertins
+ */
+public class TesteException {
+
+    public static void main(String[] args) {
+
+        int c =  4;
+
+        try {
+
+            c = c + 1;
+
+            while (c < 14) {
+
+                if (c == 13) {
+
+                    throw new Exception();
+
+                }
+
+                c = c + 3;
+
+            }
+
+        } catch (Exception e) {
+
+            c = c + 5;
+
+        } finally {
+
+            c = c + 6;
+
+        }
+
+        System.out.println("Valor= " + c);
+    }
+}
