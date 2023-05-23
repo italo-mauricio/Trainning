ce637606f9627cb1ebe4d9116e43dd6c0cb93c96
italo <italogithub@gmail.com>
2023-05-23 08:58:06 -0300

    treinando swing

--- TrainningMertins/src/main/java/com/italo/trainningmertins/exercicios/slide1/Janela.java
+++ TrainningMertins/src/main/java/com/italo/trainningmertins/exercicios/slide1/Janela.java
@@ -1,17 +1,22 @@
-
-import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 public class Janela extends JFrame {
 
-    JLabel label = new JLabel("Nome");
-    private JTextField txtNome = new JTextField(20);
-    JButton botao = new JButton("ok");
+    private JLabel label = new JLabel("Numero 1");
+    private JTextField txtNumero1 = new JTextField(20);
+    private JLabel label2 = new JLabel("Numero 2");
+    private JTextField txtNumero2 = new JTextField(20);
+    private JLabel label3 = new JLabel("Resultado");
+    private JTextField txtResultado = new JTextField(20);
+    private JButton botao = new JButton("Calcular");
+    private JComboBox<String> operacoes = new JComboBox<>(new String[]{"+", "-", "*", "/"});
 
     public Janela() {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -19,19 +24,51 @@ public class Janela extends JFrame {
     }
 
     private void iniciar() {
-        this.setLayout(new FlowLayout());
-        this.txtNome.setText("Coloque seu nome");
+        this.setLayout(new GridLayout(5, 2, 10, 10));
+
         this.add(this.label);
-        this.add(this.txtNome);
-        this.add(this.botao);
-        botao.addActionListener(new ActionListener() {
+        this.add(this.txtNumero1);
+
+        this.add(this.label2);
+        this.add(this.txtNumero2);
+
+        this.add(new JLabel("Operação"));
+        this.add(this.operacoes);
+
+        this.add(this.label3);
+        this.add(this.txtResultado);
+        this.txtResultado.setEditable(false);
+
+        this.botao.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Caixa de texto: " + txtNome.getText());
+                double num1 = Double.parseDouble(txtNumero1.getText());
+                double num2 = Double.parseDouble(txtNumero2.getText());
+                String operacao = (String) operacoes.getSelectedItem();
+                double resultado = 0;
+
+                switch (operacao) {
+                    case "+":
+                        resultado = num1 + num2;
+                        break;
+                    case "-":
+                        resultado = num1 - num2;
+                        break;
+                    case "*":
+                        resultado = num1 * num2;
+                        break;
+                    case "/":
+                        resultado = num1 / num2;
+                        break;
+                }
+
+                txtResultado.setText(String.valueOf(resultado));
             }
         });
-        this.setSize(400, 300);
 
+        this.add(this.botao);
+        this.setSize(400, 300);
     }
+
     public static void main(String[] args) {
         Janela janela = new Janela();
         janela.setVisible(true);
