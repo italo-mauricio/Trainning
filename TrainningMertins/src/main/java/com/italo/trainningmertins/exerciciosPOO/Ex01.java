2458904c541e728d09d022c9399d92986f60f52e
italo <italomauricio98@gmail.com>
2023-06-15 19:33:15 +0100

    adicionando primeiro exercicio

--- TrainningMertins/src/main/java/com/italo/trainningmertins/exerciciosPOO/Ex01.java
+++ TrainningMertins/src/main/java/com/italo/trainningmertins/exerciciosPOO/Ex01.java
@@ -9,5 +9,26 @@ package com.italo.trainningmertins.exerciciosPOO;
  * @author italo
  */
 public class Ex01 {
+    private static void method() {
+        Date dt=new Date();
+        System.out.println("Data atual: " + dt.toString());
+
+    }
+    /* 
+   
+    Failed to execute goal org.apache.maven.plugins:maven-compiler-plugin:3.10.1:compile (default-compile) on project TrainningMertins: Compilation failure: Compilation failure: 
+com/italo/trainningmertins/exerciciosPOO/Ex01.java:[13,9] cannot find symbol
+  symbol:   class Date
+  location: class com.italo.trainningmertins.exerciciosPOO.Ex01
+com/italo/trainningmertins/exerciciosPOO/Ex01.java:[13,21] cannot find symbol
+  symbol:   class Date
+  location: class com.italo.trainningmertins.exerciciosPOO.Ex01
+-> [Help 1]
+
+To see the full stack trace of the errors, re-run Maven with the -e switch.
+Re-run Maven using the -X switch to enable full debug logging.
     
+    
+    
+    */
 }
