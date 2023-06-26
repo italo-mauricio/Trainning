03fcb10654212aa052e6cde013c737fc0973bbe6
italo <italomauricio98@gmail.com>
2023-06-26 01:01:57 +0100

    update

---
+++ TrainningMertins/src/main/java/exception/ex01/Atividade1.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exception.ex01;
+
+/**
+ *
+ * @author italo
+ */
+public class Atividade1 {
+
+    public static void main(String[] args) {
+        System.out.println("inicio do main");
+        metodo1();
+        System.out.println("fim do main");
+    }
+
+    static void metodo1() {
+        System.out.println("inicio do metodo1");
+        metodo2();
+        System.out.println("fim do metodo1");
+    }
+
+    static void metodo2() {
+        System.out.println("inicio do metodo2");
+        int[] array = new int[10];
+        for (int i = 0; i <= 15; i++) {
+            array[i] = i;
+            System.out.println(i);
+        }
+        System.out.println("fim do metodo2");
+    }
+}
