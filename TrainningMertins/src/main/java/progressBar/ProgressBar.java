d0f0a449e2e35b725da50991a20f3739be619cbe
italo <italomauricio98@gmail.com>
2023-07-13 17:33:23 -0300

    update

---
+++ TrainningMertins/src/main/java/progressBar/ProgressBar.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package progressBar;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author italo
+ */
+public class ProgressBar {
+    
+    private JFrame mainFrame;
+    
+    ProgressBar(){
+        mainFrame = new JFrame("Progress Bar Exemple")
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    private class Task extends Thread{
+        
+    }
+}
