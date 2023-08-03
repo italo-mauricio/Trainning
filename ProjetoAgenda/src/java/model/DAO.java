1c2a064aa651afeaa0bd40a9985642654dbef288
italo <italomauricio98@gmail.com>
2023-08-04 00:14:52 +0100

    update

--- ProjetoAgenda/src/java/model/DAO.java
+++ ProjetoAgenda/src/java/model/DAO.java
@@ -4,10 +4,34 @@
  */
 package model;
 
+import java.sql.Connection;
+
 /**
  *
  * @author italo
  */
 public class DAO {
+    // Módulo de conexão
+     
+    // Parâmetros de conexão    
     
+    private String driver = "com.mysql.cj.jdbc.Driver";
+    private String url = "jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
+    private String user = "root";
+    private String password = "188988";
+    
+    
+    // Método conexão
+    
+    private Connection conectar(){
+        Connection con = null;
+        try {
+            Class.forName(driver);
+        } catch (Exception e) {
+        }
+        
+    }
+
+    
+     
 }
